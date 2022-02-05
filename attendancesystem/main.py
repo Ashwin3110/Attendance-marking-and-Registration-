94f7ba78b8040262d81f3d431511f78370d1f7a9
Ashwin3110 <66059199+Ashwin3110@users.noreply.github.com>
2022-02-05 13:23:33 +0530

    Update main.py

--- attendancesystem/main.py
+++ attendancesystem/main.py
@@ -8,7 +8,7 @@ Created on Thu Jan 20 12:58:51 2022
 
 choice = int(input("1.New Resgistry 2.Attendance :"))
 if(choice == 1):
-    execfile('attendance.py')
+    exec(open('register.py').read())
 else:
-    execfile('register.py')
+    exec(open('attendance.py').read())
     
